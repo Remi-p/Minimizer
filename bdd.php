0d21ece27eb61cd2c8f799de75c592f712027439
Remi Perrot <remi@thisisparachute.com>
2014-01-10 09:57:10 +0000

    Translation

--- bdd.php
+++ bdd.php
@@ -1,7 +1,17 @@
 <?php
+
+//~ Your server address (probably localhost)
 $serveur='Adresse de votre serveur mysql (probablement localhost)';
+
+//~ Your username for the database
 $login='Nom d utilisateur pour la base de donnée';
+
+//~ The password of the database
 $mdp='Mot de passe qui va avec';
+
+//~ The database itself
 $bdd='Nom de la base de donnée';
+
+//~ Connexion password for the minimizer
 $connexion='Mot de passe de connexion au minimiseur';
 ?>
